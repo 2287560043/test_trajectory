4e7749f02853e3fd8453e7607ad32cc97f9e4272
gqf74 <gqf_74@qq.com>
2025-11-19 23:27:00 +0800

    111

---
+++ gimbal_trajectory/include/gimbal_trajectory/gimbal_trajectory_node.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// ros
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/logging.hpp>
+
+
+class GimbalTrajectoryNode
+{
+public:
+    GimbalTrajectoryNode(const rclcpp::NodeOptions & options);
+    ~GimbalTrajectoryNode();
+
+
+private:
+    
+};
---
+++ gimbal_trajectory/src/gimbal_trajectory_node.cpp
@@ -0,0 +1,13 @@
+#include "../include/gimbal_trajectory/gimbal_trajectory_node.hpp"
+#include <iostream>
+
+GimbalTrajectoryNode::GimbalTrajectoryNode(const rclcpp::NodeOptions & options)
+{
+public:
+    
+    
+}
+
+GimbalTrajectoryNode::~GimbalTrajectoryNode()
+{
+}
