6e21ec222e90405b7ffa0157d1d0e9964457dd7d
ly <>
2025-12-30 18:49:31 +0800

    1

--- autoaim_bring_up/launch/autoaim.launch.py
+++ autoaim_bring_up/launch/autoaim.launch.py
@@ -236,13 +236,13 @@ def generate_launch_description():
 
     return LaunchDescription([
         ComposableNodeContainer(
-            name='camera_imu_syncer_container',
+            name='camera_imu_bridge_container',
             namespace='',
             package='rclcpp_components',
             executable='component_container_mt',
             composable_node_descriptions=[
                 ComposableNode(
-                    package='camera_imu_syncer',
+                    package='camera_imu_bridge',
                     plugin='helios_cv::CameraImuBridgeNode',
                     name='camera_imu_syncer',
                     parameters=[{
